96dbce1bbbeeaea3b1d3b77826d83c4b0e130ba9
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2022-12-21 17:04:02 +0800

    telegram login fix4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,14 +22,8 @@ export default function App(props: AppProps) {
         src="https://telegram.org/js/telegram-widget.js?21"
         data-telegram-login="Kartel_app_bot" data-size="medium" data-radius="10" data-onauth="onTelegramAuth(user)" data-request-access="write"
         strategy="lazyOnload"
-        onLoad={() => {
-          console.log("Telegram auth script loaded")
-          function onTelegramAuth(user: any) {
-            alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-          };
-        }
-        }
       />
+      <Script type="text/javascript" src="/static/script.js"></Script>
 
 
 
