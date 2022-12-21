2726cc31fbe2efeb91c3b895677215457da0d274
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2022-12-21 16:31:00 +0800

    telegram login fix

--- scripts/telegram.js
+++
@@ -1,4 +0,0 @@
-alert('test');
-function onTelegramAuth(user) {
-    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-  }
